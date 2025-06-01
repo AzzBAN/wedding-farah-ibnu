5d0a9b4d8c8d9dc2380032e4536f0dc3c706df86
Azhar Nugraha <azhar@AzzBAN-Pro.local>
2025-06-02 00:20:20 +0700

    fix api conf

--- src/api/conf.tsx
+++ src/api/conf.tsx
@@ -1,3 +1,3 @@
 const env = process.env.NODE_ENV;
 
-export const host = env === "development" ? "https://wedding-be.azzban.my.id" : "http://localhost:1323";
+export const host = env === "production" ? "https://wedding-be.azzban.my.id" : "http://localhost:1323";
